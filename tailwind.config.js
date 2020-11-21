9ae4dbe6f888b1a87871b2b89156a86cf8f86743
Supphachoke Suntiwichaya <mrchoke@gmail.com>
2020-11-21 19:40:37 +0700

    add tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true
+    },
+    fontFamily: {
+      sans: ['Prompt'],
+      serif: ['Prompt'],
+      display: ['Prompt'],
+      body: ['Prompt']
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
